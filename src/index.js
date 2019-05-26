d0a63bb3a096161447a34777c9566bf99398ae03
Hamza Halilovic <hamzahalilovic.12@gmail.com>
2019-05-26 19:13:44 +0200

    s

--- src/index.js
+++ src/index.js
@@ -7,9 +7,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" text="Nice post!"/>
-            <CommentDetail author="Alex" timeAgo="Today at 2PM" text="Good read!"/>
-            <CommentDetail author="Jane" timeAgo="Yesterday at 4PM" text="Very Good"/>
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 4:45PM"
+                text="Nice post!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Alex"
+                timeAgo="Today at 2PM"
+                text="Good read!" 
+                avatar={faker.image.avatar()}
+                />
+            <CommentDetail
+                author="Jane" 
+                timeAgo="Yesterday at 4PM" 
+                text="Very Good" 
+                avatar={faker.image.avatar()}
+                />
         </div>
 
     );
